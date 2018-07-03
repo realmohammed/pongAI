a91e3ca9fa0f0e7ff8748f3c1c9cb5b922cb347d
Mohammed Alturki <hypnos@Mohammeds-MacBook-Pro-3.local>
2018-07-02 21:22:02 -0400

    final

---
+++ PaddleAI.lua
@@ -0,0 +1,49 @@
+--[[
+    GD50 2018
+    Pong Remake
+
+    -- Paddle Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents a paddle that can move up and down. Used in the main
+    program to deflect the ball back toward the opponent.
+]]
+
+PaddleAI = Class{}
+
+--[[
+    The `init` function on our class is called just once, when the object
+    is first created. Used to set up all variables in the class and get it
+    ready for use.
+
+    Our Paddle should take an X and a Y, for positioning, as well as a width
+    and height for its dimensions.
+
+    Note that `AI` is a reference to *this* object, whichever object is
+    instantiated at the time this function is called. Different objects can
+    have their own x, y, width, and height values, thus serving as containers
+    for data. In this sense, they're very similar to structs in C.
+]]
+function PaddleAI:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
+
+function PaddleAI:updateh(dt)
+
+  if ball.y > 0 then
+    self.y = ball.y * 0.89
+  else
+    self.y = ball.y * 0.98
+  end
+end
+
+
+function PaddleAI:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
